67c8066d6e5c5aff789434f84c5253fddb328e4d
johnvincentio <john@johnvincent.io>
2019-07-01 14:37:43 -0400

    add

---
+++ udemy-course-grider/README.md
@@ -0,0 +1,45 @@
+# How to get help
+
+Twitter @ste_grider
+
+Main window = Browser window
+
+## Setup
+
+```
+npm i electron --save
+```
+
+```
+npm run electron
+```
+
+## Reload Window
+
+```
+Command R
+```
+
+do not need to restart the electron app
+
+## Toggle Developer Tools Window
+
+```
+Command Option i
+```
+
+## fluent-ffmpeg
+
+```
+which brew
+```
+
+```
+brew install ffmpeg
+```
+
+Took a lot of mucking about, but eventually.
+
+```
+ffmpeg
+```
---
+++ udemy-course-grider/course/videoinfo/index.js
@@ -0,0 +1,39 @@
+
+// runs with the nodeJS environment, does not support ES6
+
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+	mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
+		nodeIntegration: true
+	}});
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
+	mainWindow.webContents.openDevTools();
+	mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log('Video duration is:', metadata.format.duration)
+    mainWindow.webContents.send(
+      'video:metadata',
+      metadata.format.duration
+    );
+  });
+});
+
+// ipcMain.on('video:submit', (event, path) => {
+// 	console.log('ipcMain.on(video:submit)');
+// 	// ffmpeg.ffprobe(path, (err, metadata) => {
+// 	// 	console.log('Video duration is: ', metadata.format.duration);
+// 	// });
+// });
+
+
