7ed8e68bb35471668ab7b5bd9bcc73153268e67e
johnvincentio <john@johnvincent.io>
2019-07-01 18:52:49 -0400

    add

--- udemy-course-grider/course/todos/index.js
+++ udemy-course-grider/course/todos/index.js
@@ -3,9 +3,10 @@
 
 const electron = require('electron');
 
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
 	mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
@@ -13,17 +14,71 @@ app.on('ready', () => {
 	}});
 	mainWindow.loadURL(`file://${__dirname}/main.html`);
 	mainWindow.webContents.openDevTools();
-	mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+	mainWindow.on('closed', () => app.quit());
+	
+	const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
 
-// ipcMain.on('video:submit', (event, path) => {
-//   ffmpeg.ffprobe(path, (err, metadata) => {
-//     console.log('Video duration is:', metadata.format.duration)
-//     mainWindow.webContents.send(
-//       'video:metadata',
-//       metadata.format.duration
-//     );
-//   });
-// });
+const createAddWindow = () => {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+		title: 'Add New Todo',
+		webPreferences: {
+			nodeIntegration: true
+		}
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+  addWindow.on('closed', () => addWindow = null);
+};
+
+ipcMain.on('todo:add', (event, todo) => {
+  mainWindow.webContents.send('todo:add', todo);
+  addWindow.close();
+});
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Todo',
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear Todos',
+        click() {
+          mainWindow.webContents.send('todo:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if (process.platform === 'darwin') {
+	menuTemplate.unshift({ label: '' });
+}
+
+if (process.env.NODE_ENV !== 'production') menuTemplate.push({
+  label: 'Developer',
+  submenu: [
+    {role: 'reload'},
+    {
+      label: 'Toggle Developer Tools',
+      accelerator: 'Command+Option+I',
+      click(item, focusedWindow) {
+        focusedWindow.toggleDevTools();
+      }
+    }
+  ]
+});
