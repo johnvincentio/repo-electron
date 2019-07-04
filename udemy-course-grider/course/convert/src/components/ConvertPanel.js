d4ab6cedd70c9a24c4bb5bb18457b9a8815af37d
johnvincentio <john@johnvincent.io>
2019-07-04 15:46:52 -0400

    add

---
+++ udemy-course-grider/course/convert/src/components/ConvertPanel.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router'
+import * as actions from '../actions';
+
+class ConvertPanel extends Component {
+
+  onCancelPressed = () => {
+    this.props.removeAllVideos();
+    this.props.history.push('/')
+  }
+
+  convertVideos = () => {
+    this.props.convertVideos(this.props.videos);
+  }
+
+  render() {
+    return (
+      <div className="convert-panel">
+        <button className="btn red" onClick={this.onCancelPressed}>
+          Cancel
+        </button>
+        <button className="btn" onClick={this.convertVideos}>
+          Convert!
+        </button>
+      </div>
+    );
+  };
+}
+
+const mapStateToProps = state => {
+  return {
+    videos: Object.values(state.videos)
+  }
+}
+
+export default withRouter(
+  connect(mapStateToProps, actions)(ConvertPanel)
+);
