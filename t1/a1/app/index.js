18d720bdd6ead2b9be588b5223a92e7258006960
johnvincentio <john@johnvincent.io>
2019-07-05 15:20:30 -0400

    add

---
+++ t1/a1/app/index.js
@@ -0,0 +1,86 @@
+
+// runs with the nodeJS environment, does not support ES6
+
+const electron = require('electron');
+
+const { app, BrowserWindow, ipcMain, Menu } = electron;
+
+let mainWindow;
+let addWindow;
+
+app.on('ready', () => {
+	mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
+		nodeIntegration: true
+	}});
+	mainWindow.loadURL(`file://${__dirname}/main.html`);
+	mainWindow.webContents.openDevTools();
+	mainWindow.on('closed', () => app.quit());
+	
+	const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const createAddWindow = () => {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+		title: 'Add New Todo',
+		webPreferences: {
+			nodeIntegration: true
+		}
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+  addWindow.on('closed', () => addWindow = null);
+};
+
+ipcMain.on('todo:add', (event, todo) => {
+  mainWindow.webContents.send('todo:add', todo);
+  addWindow.close();
+});
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Todo',
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear Todos',
+        click() {
+          mainWindow.webContents.send('todo:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+
+
+if (process.platform === 'darwin') {
+	menuTemplate.unshift({ label: '' });
+}
+
+if (process.env.NODE_ENV !== 'production') menuTemplate.push({
+  label: 'Developer',
+  submenu: [
+    {role: 'reload'},
+    {
+      label: 'Toggle Developer Tools',
+      accelerator: 'Command+Option+I',
+      click(item, focusedWindow) {
+        focusedWindow.toggleDevTools();
+      }
+    }
+  ]
+});
